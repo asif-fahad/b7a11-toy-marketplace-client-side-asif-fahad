5f63704e81b79b52caa8a64cc95c143eea0dd80c
asif-fahad <asif123fahad@gmail.com>
2023-05-19 11:24:24 +0600

    add a toy created

--- src/pages/AddAToy/AddAToy.jsx
+++ src/pages/AddAToy/AddAToy.jsx
@@ -1,9 +1,105 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProviders';
 
 const AddAToy = () => {
+
+    const { user } = useContext(AuthContext);
+
+
     return (
-        <div>
-            add a toy
+        <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-extrabold text-center">Add a Toy</h2>
+            <form>
+                {/* form toy name and seller name*/}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Toy Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Toy Name" className="input input-bordered w-full" required />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Seller Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="sname" placeholder="Seller Name" defaultValue={user?.displayName} className="input input-bordered w-full" required />
+                        </label>
+                    </div>
+                </div>
+                {/* form seller email and subCategory */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Seller Email</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="email" name="email" placeholder="Seller Email" defaultValue={user?.email} className="input input-bordered w-full" required />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <select className="select w-full max-w-xs" name='subCategory'>
+                            <option disabled selected>Pick your favorite Toy Car</option>
+                            <option>Sports Car</option>
+                            <option>Truck</option>
+                            <option>Fire Truck</option>
+                        </select>
+                    </div>
+                </div>
+                {/* form price and rating row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="price" placeholder="Price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Rating</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="rating" placeholder="Rating" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form quantity and details row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="quantity" placeholder="quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Details Description</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="details" placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form Photo url row */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Add Toy" className="btn btn-block" />
+
+            </form>
         </div>
     );
 };
